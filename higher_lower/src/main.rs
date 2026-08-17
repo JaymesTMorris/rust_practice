5ed1e976dbf2777af871dc786983131eb8b995b3
JaymesTMorris <jaymestmorris16@gmail.com>
2026-08-17 07:10:51 -0400

    add user input

--- higher_lower/src/main.rs
+++ higher_lower/src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
+use std::io::Write; // the trait that flush() is in
+
 fn main() {
-    println!("Hello, world!");
+    // setup vars
+    let target: i32 = 76;
+
+    print_starting_text();
+    game_loop();
+    print_ending_text();
+}
+
+fn print_starting_text() -> () {
+    println!("Welcome to Higher-Lower!");
+    println!("Try to guess the number that I'm thinking of.");
+    println!("");
+}
+
+fn print_ending_text() -> () {
+    println!("You won!");
+    println!("Please re-load the game to play again.");
+}
+
+fn get_user_input() -> String {
+    let mut return_val: String = String::new();
+    print!("Enter a number [1-100]: ");
+    io::stdout().flush().expect("flushed");
+    io::stdin()
+        .read_line(&mut return_val)
+        .unwrap();
+    return_val = return_val.trim().to_string();
+    return return_val;
+}
+
+fn game_loop () -> () {
+    loop {
+        let user_input = get_user_input();
+        println!("{user_input}");
+        break;
+    }
 }
